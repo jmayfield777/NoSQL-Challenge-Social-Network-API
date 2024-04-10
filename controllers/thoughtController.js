a0a6322d7cf3c2addb32c3a54a7b6cc5a0a37dfe
Josh <josh.mayfield45@gmail.com>
2024-04-09 22:04:18 -0400

    added reaction routes to thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -85,6 +85,41 @@ module.exports = {
     } catch {
       res.status(500).json(err);
     }
-  }
+  },
 
-}
+  async addReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that Id!' });
+      }
+
+      res.json(thought);
+    } catch {
+      res.status(500).json(err);
+    }
+  },
+
+  async removeReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that Id!' });
+      }
+
+      res.json(thought);
+    } catch {
+      res.status(500).json(err);
+    }
+  },
+};
